adf6f0064e70351675cae4ddb6090f164290b5f0
Gurkirat3162 <38437778+Gurkirat3162@users.noreply.github.com>
2020-09-06 12:44:20 +0530

    Add files via upload

---
+++ Signup.php
@@ -0,0 +1,61 @@
+
+<html >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<title>Sign up</title>
+<link href="Style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body class="style">
+
+
+<h1><center>Secret Diary</center></h1>
+<p1><center>Store your thoughts permanently and securely.</center></p1><br></br>
+<p2><center>Interested ? Sign up now. </center></p2>
+
+<table align="center">
+<form method ="post" action="Connection.php" />
+
+<tr>
+<td  >
+
+<input type="text" name="username" placeholder="Your Email" required/ class="style1"></td><br /><br /><br /><br />
+
+<tr>
+<td ><br /> 
+  <input type="password" name="password" placeholder="Password" required/ class="style1"  >
+
+</td>
+</tr>
+
+
+</tr>
+
+<tr>
+<td><br></br>
+
+<input type="checkbox" > Stay logged in</input>
+</td>
+</tr>
+<tr>
+<td><br /><br/>
+<input type="submit" name="Sign up"  value="Sign up!"  / class="contain">
+</td>
+</tr>
+
+
+ <tr> 
+ <td> 
+
+<a href="Login.php"/> Log in 
+</td> 
+ </tr> 
+</form>
+</body>
+</html>
+
+		
+
+			
+			
